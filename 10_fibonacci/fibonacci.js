cdc194c2711626e754e1f5d09f896162fcd49688
nknaeppe <nilsknaepper@gmail.com>
2022-03-23 02:36:55 +0100

    Fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+function fibonacci(x) {
 
+    if (isNaN(x) || x < 0) {
+        return "OOPS"
+    }
+    if (x == 0) {
+        return 0;
+    }
+    if (x == 1) {
+        return 1;
+    }
+    return (fibonacci(x-1) + fibonacci(x-2));
 };
+
+console.log(fibonacci("Eren"));
 
 // Do not edit below this line
 module.exports = fibonacci;
