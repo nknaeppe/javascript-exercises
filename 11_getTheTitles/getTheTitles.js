d4a82db125688da34a28dac0c69676072468c80c
nknaeppe <nilsknaepper@gmail.com>
2022-03-24 02:19:24 +0100

    getTheTitles finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let output = [];
+    for (let i = 0; i < books.length; i++){
+        output.push(books[i].title);
+        };
+    return output;
 };
+    const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
 
+console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
